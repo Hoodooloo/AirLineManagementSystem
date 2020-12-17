821871c9b99cf73f7d76bb4f20496d97d6e8cbc8
Bhaskar Sharma <bhaskar.shasank3333@gmail.com>
2020-12-17 19:54:21 +0530

    First commit

---
+++ airline_user.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include "admin.h"
+using namespace std;
+
+int main(){
+	int emp_user;
+	cout<< "------------------------------------------------------------"<<endl;
+	cout<< "------------------------------------------------------------"<<endl;
+	printf("\t Enter 1 to continues as User(To Book Tickets)\n");
+	printf("\t Enter 2 to continues as admin.\n");
+	cin>> emp_user;
+	if (emp_user == 2)
+	{
+		string pass;
+		cout << "Enter Admin Password: "<< endl;
+		cin >> pass;
+		while(pass != "Hoodooloo"){
+			cout << "Invalid Pass Word Please Try Again: " << endl;
+			cin >> pass;
+		}
+		admin();
+
+	}
+}
